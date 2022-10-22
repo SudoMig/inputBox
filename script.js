134c181b6333f379e92060001fd2519a6c245dbd
sudomig <mu_@tuta.io>
2022-10-22 12:07:26 -0400

    html & css complete

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+function displayText() {
+    let userInput = document.getElementById('user-input').value
+    document.getElementById('user-output').innerHTML = userInput
+    console.log('clicked ------->', userInput );
+
+
+}
